fb5d34bc7b0d0a4a2c517bfd9270f5cd047d6e40
kanak <kanakkshk@gmail.com>
2021-07-30 22:16:09 +0530

    master

--- lib/product_screen/product_sreen.dart
+++ lib/product_screen/product_sreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProductScreen extends StatelessWidget {
   const ProductScreen({Key? key}) : super(key: key);
@@ -35,6 +36,117 @@ class ProductScreen extends StatelessWidget {
                   bottomLeft: Radius.circular(30),
                   bottomRight: Radius.circular(30),
                 ),
+              ),
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Brand',
+                            style: GoogleFonts.roboto(
+                                fontSize: 16,
+                                color: Color(0xff8B2833),
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Moon',
+                            style: GoogleFonts.roboto(
+                              fontSize: 15,
+                              color: Color(0xff8B2833).withOpacity(0.5),
+                            ),
+                          ),
+                          SizedBox(
+                            height: size.height * 0.05,
+                          ),
+                          Text(
+                            'Code',
+                            style: GoogleFonts.roboto(
+                                fontSize: 16,
+                                color: Color(0xff8B2833),
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Y2d345',
+                            style: GoogleFonts.roboto(
+                              fontSize: 15,
+                              color: Color(0xff8B2833).withOpacity(0.5),
+                            ),
+                          ),
+                          SizedBox(
+                            height: size.height * 0.05,
+                          ),
+                          Text(
+                            'Leather',
+                            style: GoogleFonts.roboto(
+                                fontSize: 16,
+                                color: Color(0xff8B2833),
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            '100 %',
+                            style: GoogleFonts.roboto(
+                              fontSize: 15,
+                              color: Color(0xff8B2833).withOpacity(0.5),
+                            ),
+                          ),
+                          SizedBox(
+                            height: size.height * 0.05,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Image.asset(
+                        'assets/pinkbag.png',
+                        width: size.width * 0.6,
+                        height: size.height * 0.6,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Pink Bag',
+                    style: GoogleFonts.roboto(
+                      fontSize: 32,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '₹' + ' 889',
+                        style: GoogleFonts.roboto(
+                            fontSize: 16,
+                            color: Color(0xffC7051A),
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.lineThrough),
+                      ),
+                      SizedBox(
+                        width: size.width * 0.04,
+                      ),
+                      Text(
+                        '₹' + ' 789',
+                        style: GoogleFonts.roboto(
+                            fontSize: 20,
+                            color: Color(0xff060608),
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
