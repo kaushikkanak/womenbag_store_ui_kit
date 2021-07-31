cba6d0e0033a949fbd3576d1f81a54b18d33b20d
kanak <kanakkshk@gmail.com>
2021-07-31 10:29:26 +0530

    master

--- lib/product_screen/product_sreen.dart
+++ lib/product_screen/product_sreen.dart
@@ -14,8 +14,7 @@ class ProductScreen extends StatelessWidget {
       appBar: AppBar(
         elevation: 0.0,
         backgroundColor: Color(0xffFFB9C4),
-        leading: Icon(
-          Icons.arrow_back,
+        iconTheme: IconThemeData(
           color: Color(0xffC7051A),
         ),
         actions: [
@@ -161,7 +160,95 @@ class ProductScreen extends StatelessWidget {
                     fontWeight: FontWeight.bold),
               ),
             ),
-            SizeRadio()
+            SizeRadio(),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'About',
+                style: GoogleFonts.roboto(
+                    fontSize: 16,
+                    color: Color(0xff060608),
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Occaecat qui sit quis labore reprehenderit nulla. Amet pariatur voluptate laboris ipsum veniam exercitation est do duis quis laborum reprehenderit aute.',
+                style: GoogleFonts.roboto(
+                  fontSize: 14,
+                  color: Color(0xff060608),
+                ),
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(29.5),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        '+',
+                        style: GoogleFonts.roboto(
+                          fontSize: 30,
+                          color: Color(0xffC7051A),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white, shape: CircleBorder()),
+                    ),
+                  ),
+                  Text('1'),
+                  Container(
+                    alignment: Alignment.topRight,
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(29.5),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        '-',
+                        style: GoogleFonts.roboto(
+                          fontSize: 30,
+                          color: Color(0xffC7051A),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white, shape: CircleBorder()),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Add to Cart',
+                      style: GoogleFonts.roboto(
+                        fontSize: 14,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      elevation: 2,
+                      padding: EdgeInsets.symmetric(
+                          horizontal: size.width * 0.2,
+                          vertical: size.height * 0.02),
+                      primary: Color(0xffC7051A),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
